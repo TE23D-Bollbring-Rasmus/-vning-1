f6166e64a7e8eeddf35a4ce20e5afcd039d2fe58
Nils Rasmus Mattsson Bollbring <rasmus.mattssonbollbring@elev.ga.ntig.se>
2024-11-11 08:55:28 +0100

    jhg

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿//if (6>=3){
+//Console.WriteLine("Hello, World!");  
+//}
+
+//Console.ReadLine();
+
+
+// Console.WriteLine("skriv in ditt användarnamn och lösenord");
+
+
+// string användarnamn = Console.ReadLine();
+// string lösenord = Console.ReadLine();
+
+// if (användarnamn != "kalleanka" && lösenord != "12345"){
+//     Console.WriteLine("nej");
+// }
+// else{
+//     Console.WriteLine("Välkommen");
+// }
+// Console.ReadLine();
+
+
+
+// for (int i = 0; i<32 ; i++){
+//   Console.WriteLine("Hello World");
+// }
+
+// Console.ReadLine();
+
+Console.WriteLine("Skriv in lösenordet");
+string lösenord = Console.ReadLine();
+
+while ()
